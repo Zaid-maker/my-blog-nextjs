11501994e344765aa2f41393bf92065d15185f05
Zaid-maker <pzhafeez@gmail.com>
2023-03-25 02:06:15 +0500

    ✨ feat([id].tsx): add dynamic post page with getStaticPaths and getStaticProps functions

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,17 +1,24 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
-import Layout from "../../components/Layout";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-import { getAllPostIds, getPostData } from "../../lib/posts";
-
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
@@ -23,22 +30,19 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticProps({ params }) {
-  // Add the "await" keyword like this:
-  const postData = await getPostData(params.id);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
   return {
     props: {
       postData,
     },
   };
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
+};
