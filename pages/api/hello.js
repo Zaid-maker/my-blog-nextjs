b36427617420c519e3063cbf5937010e5fe2a780
Zaid-maker <pzhafeez@gmail.com>
2023-03-25 02:08:51 +0500

    🔨 refactor(hello.js): change function declaration to arrow function and add types to parameters

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: "Hello" });
-}
+};
